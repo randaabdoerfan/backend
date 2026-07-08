17d90365e4dd4fbb5b6526916abbfba739c2482f
Randa <randaerfan12@gmail.com>
2026-07-08 18:31:59 +0300

    email

--- config/email.config.js
+++ config/email.config.js
@@ -2,13 +2,15 @@ const nodemailer = require('nodemailer')
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    service: "gmail",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.email_user || 'randaerfan12@gmail.com',
         pass: process.env.email_password || 'jebk lvnj ptpv oivo'
-    }
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 15000
 })
 
 module.exports = transporter
